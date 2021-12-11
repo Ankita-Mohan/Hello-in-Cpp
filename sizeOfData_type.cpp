e9c983b0c5899d44286df89f77c73ab76ce50430
Ankita Mohan <95054635+Ankita-Mohan@users.noreply.github.com>
2021-12-11 23:33:26 +0530

    sizeOfData_type.cpp

---
+++ sizeOfData_type.cpp
@@ -0,0 +1,17 @@
+#include <iostream> //pre-processor directive to include iostream
+
+using namespace std;
+int main()  //main function with no arguments
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    cout << "Size of char: " << sizeof(c) << " byte" << endl;
+    cout << "Size of int: " << sizeof(a) << " bytes" << endl;
+    cout << "Size of float: " << sizeof(b) << " bytes" << endl;
+    cout << "Size of double: " << sizeof(d) << " bytes" << endl;
+
+    return 0;
+}
+//created by Ankita Mohan
