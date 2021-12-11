d2fcf9f205de6bfc4bcce9b1171b8f6bfffa9601
Ankita Mohan <95054635+Ankita-Mohan@users.noreply.github.com>
2021-12-11 23:37:44 +0530

    product.cpp

---
+++ product.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    float a, b;
+    cout << "Enter two numbers:\n";
+    cin >> a >> b;
+    cout << "Product =" << a * b; //use of arithmetic operator
+
+    return 0;
+}
+//created by Ankita Mohan
