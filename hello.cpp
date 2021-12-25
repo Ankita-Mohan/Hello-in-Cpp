67d555b36a63caba79c57dd291d9053eb48cb82d
Ankita <ankitamohan1267@gmail.com>
2021-12-25 18:36:51 +0530

    changing hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,4 @@
-#include<iostream> /*where "#" is "directive", 
+#include <iostream> /*where "#" is "directive", 
                      "include" states the "directive to include the filename"
                      "#include" combined is known as "Pre-Processor Directive"
                      "iostream" where "i" stands for "input"
@@ -7,12 +7,12 @@
 
 using namespace std; //used to include all files in iostream but it occupies a lot of memory:-(
 
-int main()     /*main function--> from where the compilation starts
+int main() /*main function--> from where the compilation starts
                  "int" is the data type of the returned value
                  () -->these are known as parentheses and here it has no arguments*/
 {
-  cout<<"Hello Guys"<<endl;  //cout--> spelled as "see-out" is used to display the text written inside double inverted commas ("")
-  return 0;                  //retuns the value 0 to the main function
-}    //these are known as "braces"
+  cout << "Hello friends" << endl; //cout--> spelled as "see-out" is used to display the text written inside double inverted commas ("")
+  return 0;                        //retuns the value 0 to the main function
+} //these are known as "braces"
 
 //created by Ankita Mohan
